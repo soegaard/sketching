9bcb69d9e3d9a31b90ad7c63bdd3d8a5592158dd
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-07-04 17:28:04 +0200

    Add sketching package

---
+++ sketching/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps    '("sketching-lib" "sketching-examples" "sketching-doc"))
+(define implies '("sketching-lib" "sketching-examples" "sketching-doc"))
+
+(define pkg-desc "Drawing and animation library inspried by Processing.")
+
+(define pkg-authors '(soegaard))
+
+(define test-responsibles '((all jensaxel@soegaard.net)))
