303111953f2b6b5b7d293536317c77260c362736
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-07-04 16:53:30 +0200

    Add info.rkt for sketching-examples

---
+++ sketching-examples/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'single)
+
+(define build-deps '("sketching-lib"))
+
+(define update-implies '("sketching-lib"))
+
+(define pkg-desc "collection of Sketching examples")
+
+(define pkg-authors '(soegaard))
