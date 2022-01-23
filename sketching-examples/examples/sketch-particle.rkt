c803096f03e61c35912aaa51318742eae11d3d65
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-01-23 10:12:27 +0100

    New colors

--- sketching-examples/examples/sketch-particle.rkt
+++ sketching-examples/examples/sketch-particle.rkt
@@ -10,12 +10,12 @@
   (match-define (particle x y vx vy ax ay α) p)
   (particle (+ x vx) (+ y vy) (+ vx ax) (+ vy ay) ax ay (max (- α 2) 0)))
 
-(define (new-particle [x 300] [y 300] [vx (random -1 1)] [vy (random -5 -1)] [ax 0] [ay 0.1])
+(define (new-particle [x 300] [y 300] [vx (random -1 1)] [vy (random -1 -10)] [ax 0] [ay 0.1])
   (particle x y vx vy ax ay 255))
 
 (define (draw-particle p)
   (match-define (particle x y vx vy ax ay α) p)
-  (fill 255 α)
+  (fill (random 255) 255 255 α)
   (circle x y 5))
 
 (define particles '())
@@ -29,8 +29,9 @@
                 (map update-particle particles))))
 
 (define (setup)
-  (frame-rate 120)
-  (size 600 400))
+  (frame-rate 30)
+  (size 600 400)
+  (color-mode 'hsb 255 255 255))
 
 (define (mouse-pressed)
   (for ([i 10])
@@ -42,4 +43,8 @@
   (no-stroke)
   (map draw-particle particles)
   (update-particles!)
-  (add-particle! (new-particle 300 200)))
+  (add-particle! (new-particle 100 400))
+  (add-particle! (new-particle 200 400))
+  (add-particle! (new-particle 300 400))
+  (add-particle! (new-particle 400 400))
+  (add-particle! (new-particle 500 400)))
